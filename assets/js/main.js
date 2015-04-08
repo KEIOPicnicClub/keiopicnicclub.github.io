158db08268f56fc93b262f2626b99bf54cf285a4
Pen-Guin <penguin4js@gmail.com>
2015-04-08 11:21:46 +0900

    css defined ios/not ios

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,17 @@
 				$('#theMenu').toggleClass('menu-open');
 			});
 
+			var agent = navigator.userAgent;
+			if(agent.search(/iPhone/) != -1 || agent.search(/iPad/) != -1) {
+				console.log("ios");
+			}
+			else {
+				console.log("not ios");
+					$('#headerwrap').css({backgroundAttachment : 'fixed'});
+					$('#subwrap0').css({backgroundAttachment : 'fixed'});
+					$('#subwrap1').css({backgroundAttachment : 'fixed'});
+					$('#subwrap2').css({backgroundAttachment : 'fixed'});
+			}
+
 
 })(jQuery)
